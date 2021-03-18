6780a4a07de25135ebb2d2607b767317a33de321
Adithya.Viswanathan <adithya.viswanathan@ibm.com>
2021-03-18 09:30:10 +0530

    Product Route Added

---
+++ api/v1/products/routes.js
@@ -0,0 +1,31 @@
+const argon2 = require("argon2");
+const router = require("express").Router();
+
+const Product = require("./model/products.entity");
+
+router.route("/")
+.get(async (req, res) => {
+    const products = await Product.findAll();
+
+    return res.status(200).json({
+      ok: true,
+      products,
+    });
+  })
+  .post(async (req, res) => {
+    const { name, price, category, owner_id } = req.body;
+
+    const product = await Product.create({
+      name,
+      price,
+      owner_id,
+      category,
+    });
+
+    return res.status(201).json({
+      ok: true,
+      product,
+    });
+  });
+
+module.exports = router;
