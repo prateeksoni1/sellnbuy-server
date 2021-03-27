133380553155e3200fbf8686a670ed1b58e67b97
Adithya.Viswanathan <adithya.viswanathan@ibm.com>
2021-03-27 10:18:21 +0530

    Admin Reject

--- api/v1/superadmin/controllers.js
+++ api/v1/superadmin/controllers.js
@@ -40,3 +40,26 @@ exports.approveRequest = async (req, res, next) => {
     return next(err);
   }
 };
+
+exports.rejectRequest = async (req, res, next) => {
+  try {
+    const { userId } = req.body;
+    const user = await User.findByPk(userId);
+
+    if (!user) {
+      return res.status(404).json({
+        ok: false,
+        message: 'No such user exists',
+      });
+    }
+    user.approved = false;
+    user.userType = "user";
+    await user.save();
+
+    return res.json({
+      ok: true,
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
--- api/v1/superadmin/routes.js
+++ api/v1/superadmin/routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { isSuperAdmin, checkAuthStatus } = require('../users/controllers');
-const { getAdminRequests, approveRequest } = require('./controllers');
+const { getAdminRequests, approveRequest, rejectRequest } = require('./controllers');
 
 router.get('/', checkAuthStatus, isSuperAdmin, getAdminRequests);
 router.post('/approve', checkAuthStatus, isSuperAdmin, approveRequest);
+router.post('/reject', checkAuthStatus, isSuperAdmin, rejectRequest);
 
 module.exports = router;
